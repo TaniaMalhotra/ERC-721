ea615099641adcc3aa63234b27ddfc501a8ed133
Tania <malhotratanya847@gmail.com>
2023-12-08 12:48:35 +0530

    approve whitelists to mint for free, linked to whitelist.sol

--- NFTee.sol
+++ NFTee.sol
@@ -4,26 +4,52 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./whitelist.sol";
 
 contract Buildoor is ERC721, ERC721Enumerable, Ownable {
     uint256 private _nextTokenId;
     uint256 mintRate = 0.01 ether;
-    uint public MAX_Supply=1;
-    constructor(address initialOwner)
-        ERC721("Buildoor", "BLD")
-        Ownable(initialOwner)
-    {}
+    //max of NFTS that can be minted = 10
+    uint public MAX_Supply=10;
+
+    // Whitelist contract instance
+    Whitelist whitelist;
+
+    // Number of tokens reserved for whitelisted members
+    uint256 public reservedTokens;
+    uint256 public reservedTokensClaimed = 0;
+    
+    constructor(address initialOwner, address whiteListContract) ERC721("Buildoor", "BLD") Ownable(initialOwner)
+    {
+        whitelist = Whitelist(whiteListContract);
+        reservedTokens = whitelist.MAX_ALLOWED();
+    }
 
     function _baseURI() internal pure override returns (string memory) {
         return "https://api.mynftc.com/tokens/";
     }
     //aybody can call and mint this nft. We added payable, to ask user to enter a value while callng this function
     function safeMint(address to) public payable{
+        //NFTs allowed to be minted for non whitelisted people < max_supply
+        require(totalSupply() + (reservedTokens-reservedTokensClaimed) < MAX_Supply, "EXCEEDED_MAX_SUPPLY" );
+
+        if(whitelist.whitelisted_addresses(to)){
+            require(balanceOf(to) == 0, "already minted an NFT. Can't mint more");
+            reservedTokensClaimed++;
+        } 
+        else{
+            require(msg.value >= mintRate, "not enough ether sent");
+        }
         require(totalSupply()<MAX_Supply,"Can't mint more");
-        require(msg.value>= mintRate, "Not enough ether sent");
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
     }
+
+    //people in whitelist
+    // -> they should not already own any NFT
+    // -> they don't need to pay
+    //others
+    // -> pay
 
     // The following functions are overrides required by Solidity.
 

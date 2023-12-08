9b774a31a69175bf503fd730b9487c3accbefe3a
Tania <malhotratanya847@gmail.com>
2023-12-08 12:49:45 +0530

    counter error resolved

--- whitelist.sol
+++ whitelist.sol
@@ -2,21 +2,19 @@
 pragma solidity >=0.6.12 <0.9.0;
 
 contract Whitelist{
-    mapping (address=>bool) public whitelist;
+    mapping (address=>bool) public whitelisted_addresses;
     uint8 counter = 0;
-    uint8 MAX_ALLOWED = 0;
+    uint8 public MAX_ALLOWED = 0;
     constructor(uint8 max)
     {
         MAX_ALLOWED = max;
     }
     function addUser() public
     {
-        if(counter<MAX_ALLOWED){
-        whitelist[msg.sender] = true;
         counter++;
-        }
+        require(counter<MAX_ALLOWED, "more addresses can't be whitelisted");
+        whitelisted_addresses[msg.sender] = true;
+      
     }
 }
 
-//deployed at 0x3328358128832A260C76A4141e19E2A943CD4B6D
-//4 whitelists allowed
